289b599728f186f452e85a3fedadd768c9046833
swetaas2000 <78067532+swetaas2000@users.noreply.github.com>
2021-04-26 18:46:22 +0530

    Add files via upload

---
+++ trial_2/mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(ui->radioButton->isChecked())
+       {
+           QMessageBox::information(0,"AGE","AGE IS ABOVE 18");
+       }
+
+       if(ui->radioButton_2->isChecked())
+       {
+           QMessageBox::information(0,"AGE","AGE IS BELOW 18");
+       }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    if(ui->checkBox->isChecked())
+       {
+           QMessageBox::information(0,"FRUITS","MANGOES");
+       }
+
+       if(ui->checkBox_2->isChecked())
+       {
+           QMessageBox::information(0,"FRUITS","APPLES");
+       }
+       if(ui->checkBox_3->isChecked())
+       {
+           QMessageBox::information(0,"FRUITS","GRAPES");
+       }
+}
