ebf445e20ad519915c844fce561c8081ff1c4758
swetaas2000 <78067532+swetaas2000@users.noreply.github.com>
2021-04-26 18:50:46 +0530

    Add files via upload

---
+++ TRIALS_1/main.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow1trials.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow1TRIALS w;
+    w.show();
+    return a.exec();
+}
